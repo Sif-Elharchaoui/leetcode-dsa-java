f5d1a6732c4263a42a972d154866e50cc165a506
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-24 20:03:50 +0800

    solve: Move Zeroes - in-place two-pass array manipulation

--- src/arrays/MoveZeroes.java
+++ src/arrays/MoveZeroes.java
@@ -1,4 +1,17 @@
 package arrays;
 
 public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
 }
