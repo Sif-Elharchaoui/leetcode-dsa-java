381bd1c0143fae351dae59ccf8c79dc1cab62f72
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-25 20:29:43 +0800

    solve: Remove Duplicates from Sorted Array - slow/fast pointer

--- src/two_pointers/RemoveDuplicates.java
+++ src/two_pointers/RemoveDuplicates.java
@@ -1,4 +1,17 @@
 package two_pointers;
 
 public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 1;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
 }
