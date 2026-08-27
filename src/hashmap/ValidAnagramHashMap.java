0be4e5c210054305c53e911e1b71cda320c5845c
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-27 20:47:51 +0800

    solve: Valid Anagram revisited - HashMap with getOrDefault

--- src/hashmap/ValidAnagramHashMap.java
+++ src/hashmap/ValidAnagramHashMap.java
@@ -1,4 +1,26 @@
 package hashmap;
 
+import java.util.HashMap;
+
 public class ValidAnagramHashMap {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!counts.containsKey(c) || counts.get(c) == 0) {
+                return false;
+            }
+            counts.put(c, counts.get(c) - 1);
+        }
+
+        return true;
+    }
 }
