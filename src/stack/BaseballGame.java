f184d38efcef746ecb6b4d74de6a4b94c16b0b5f
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-31 02:34:08 +0800

    solve: Baseball Game - stack application

--- src/stack/BaseballGame.java
+++ src/stack/BaseballGame.java
@@ -1,4 +1,31 @@
 package stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class BaseballGame {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String op : operations) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int second = stack.peek();
+                stack.push(top);
+                stack.push(top + second);
+            } else if (op.equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+        return sum;
+    }
 }
