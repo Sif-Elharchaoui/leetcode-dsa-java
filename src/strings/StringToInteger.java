11f943191539e4ae235c7a7ec403b937fb6f9c4a
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-01 19:33:05 +0800

    solve: String to Integer (atoi) - robust parsing with overflow handling

---
+++ src/strings/StringToInteger.java
@@ -0,0 +1,25 @@
+package strings;
+
+public class StringToInteger {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length();
+        while (i < n && s.charAt(i) == ' ') i++;
+        if (i == n) return 0;
+
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+
+        long result = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            if (result * sign >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (result * sign <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+
+        return (int) (result * sign);
+    }
+}
