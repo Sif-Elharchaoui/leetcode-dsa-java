a64fba5c2214d9d6b25ff60b6f65aac1c9f18596
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-02 22:29:06 +0800

    solve: Longest Substring Without Repeating Characters - variable window + HashSet

--- src/slidingwindow/LongestSubstringNoRepeat.java
+++ src/slidingwindow/LongestSubstringNoRepeat.java
@@ -1,4 +1,23 @@
 package slidingwindow;
 
+import java.util.HashSet;
+
 public class LongestSubstringNoRepeat {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> seen = new HashSet<>();
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            while (seen.contains(c)) {
+                seen.remove(s.charAt(left));
+                left++;
+            }
+            seen.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
 }
