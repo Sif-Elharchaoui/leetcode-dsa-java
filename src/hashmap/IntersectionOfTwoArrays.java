59eb115e26c45aa0f815bc0afaf0f44e778c6c68
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-05 23:46:34 +0800

    solve: Intersection of Two Arrays II - HashMap counting

--- src/hashmap/IntersectionOfTwoArrays.java
+++ src/hashmap/IntersectionOfTwoArrays.java
@@ -1,4 +1,28 @@
 package hashmap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class IntersectionOfTwoArrays {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        for (int num : nums1) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums2) {
+            if (counts.getOrDefault(num, 0) > 0) {
+                result.add(num);
+                counts.put(num, counts.get(num) - 1);
+            }
+        }
+
+        int[] arr = new int[result.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = result.get(i);
+        }
+        return arr;
+    }
 }
