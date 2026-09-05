70faa1966eb8fc22b62ed7510a61a593896a6832
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-05 23:46:34 +0800

    solve: Unique Number of Occurrences - HashMap + HashSet combo

--- src/hashmap/UniqueOccurrences.java
+++ src/hashmap/UniqueOccurrences.java
@@ -1,4 +1,21 @@
 package hashmap;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class UniqueOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        HashSet<Integer> seenCounts = new HashSet<>();
+        for (int count : counts.values()) {
+            if (!seenCounts.add(count)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
