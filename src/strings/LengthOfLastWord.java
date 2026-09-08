0429425dd916f605cab62b9a1ea41f6c77826020
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-08 23:31:53 +0800

    solve: Length of Last Word - trailing space handling

--- src/strings/LengthOfLastWord.java
+++ src/strings/LengthOfLastWord.java
@@ -1,4 +1,16 @@
 package strings;
 
 public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        int length = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
 }
