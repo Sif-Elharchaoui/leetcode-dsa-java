acf2b026d2a9e09abfb0a328b2390f5d25c9da15
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-11 22:45:36 +0800

    solve: Single Number - XOR trick, O(1) space

--- src/arrays/SingleNumber.java
+++ src/arrays/SingleNumber.java
@@ -1,4 +1,11 @@
 package arrays;
 
 public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
 }
