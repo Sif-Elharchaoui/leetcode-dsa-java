0b6f031a0d31d7850a161d6ee7f9d5123452d294
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-09-15 00:00:55 +0800

    solve: Find the Index of the First Occurrence in a String - brute force substring search

--- src/strings/FindSubstring.java
+++ src/strings/FindSubstring.java
@@ -1,4 +1,14 @@
 package strings;
 
 public class FindSubstring {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
